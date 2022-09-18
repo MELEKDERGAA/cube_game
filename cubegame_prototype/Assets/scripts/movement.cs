9c80e6603bf7a2aeace8bece681c23cd2430cce8
-Arthur- <47183831+MELEKDERGAA@users.noreply.github.com>
2022-09-18 21:55:43 +0100

    Update movement.cs

--- cubegame_prototype/Assets/scripts/movement.cs
+++ cubegame_prototype/Assets/scripts/movement.cs
@@ -20,20 +20,27 @@ public class movement : MonoBehaviour
     {
         //x = Input.GetAxis("Horizontal");
         //detect if the cube fall off or the force is not reseted yet
-        if (collision.gameObject.CompareTag("ground")||force!=5.0f||collision.gameObject.CompareTag("obs"))
+        if (collision != null)
         {
-            force = 0.0f;
-            transform.position = respawnpoint;
-            
-            // makes the cube after respawning hard to get right or left because  the force is too high and the mass is small
-            GetComponent<Rigidbody>().drag = 2.5f;
-            i-= Time.deltaTime;
-            //it's like a clock when "i" gets <=0 that means it's been a 1sec from respawning and only after 1 sec the cube can move freely
-            if (i<= 0.0f)
+            if (collision.gameObject.CompareTag("ground") || force != 5.0f || collision.gameObject.CompareTag("obs"))
             {
-                    i = 1.0f;
+                force = 0.0f;
+                transform.position = respawnpoint;
+                //Debug.Log(force);
+                // makes the cube after respawning hard to get right or left because  the force is too high and the mass is small
+                GetComponent<Rigidbody>().drag = 2.5f;
+                i -= Time.deltaTime;
+                //Debug.Log(i);
+                //it's like a clock when "i" gets <=0 that means it's been a 1sec from respawning and only after 1 sec the cube can move freely
+                if (i <= 0.0f)
+                {
+                    i = 2.0f;
+                    Debug.Log(i);
                     force = 5.0f;
+                    Debug.Log(force);
                     GetComponent<Rigidbody>().drag = .0f;
+                    collision = null;
+                }
             }
         }
     }

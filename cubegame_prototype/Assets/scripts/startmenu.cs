bd1bd2048999822b66311554789b50d480db4469
-Arthur- <47183831+MELEKDERGAA@users.noreply.github.com>
2022-09-24 19:39:02 +0100

    update melek

--- cubegame_prototype/Assets/scripts/startmenu.cs
+++ cubegame_prototype/Assets/scripts/startmenu.cs
@@ -5,8 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class startmenu : MonoBehaviour
 {
+    public GameObject game;
+    public GameObject pausedbutton;
+    bool ispaused;
+    private void Start()
+    {
+        ispaused = false;
+    }
     public void Start_menu()
     {
         SceneManager.LoadScene("start",LoadSceneMode.Single);
     }
+    public void Restart()
+    {
+       Scene scene=SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name,LoadSceneMode.Single);
+    }
+    public void paused() 
+    {
+        ispaused = true;   
+    }
+    public void Resume()
+    {
+        ispaused=false;
+    }
+    private void Update()
+    {
+        if (ispaused)
+        {
+            Time.timeScale = 0.0f;
+        }else
+        {
+            Time.timeScale = 1.0f;
+        }
+        if (game != null && pausedbutton != null)
+        {
+            game.SetActive(ispaused);
+            pausedbutton.SetActive(!ispaused);
+        }
+    }
+
 }
